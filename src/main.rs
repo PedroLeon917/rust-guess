ad4f42e3ebdd44fb5b95a563345a62d595a1b186
PedroLeon917 <pedro.leon@outlook.com>
2024-05-17 23:43:27 -0400

    Multiple guesses

--- src/main.rs
+++ src/main.rs
@@ -39,10 +39,54 @@ fn main() {
     // println!("{}", message)
 
 //3------------------------------------------------------------------------------
-    let correct = rand::thread_rng().gen_range(1..=10);
-    // println!("Correct: {correct}");
+    // let correct = rand::thread_rng().gen_range(1..=10);
+    // // println!("Correct: {correct}");
+    // println!("Hey, guess a number 1-10:");
+    // loop {
+    //     let mut guess = String::new();
+    //     io::stdin()
+    //         .read_line(&mut guess)
+    //         .expect("Failed to read line");
+
+    //     let guess: u32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(e) => {
+    //             println!("Error with parse, try again. {e}");
+    //             continue;
+    //         }
+    //     };
+    
+    //     match guess.cmp(&correct) {
+    //         Ordering::Greater => println!("You guessed too high."),
+    //         Ordering::Less => println!("You guessed too low."),
+    //         Ordering::Equal => {
+    //             println!("You guessed CORRECT");
+    //             break;
+    //         }  
+    //     };
+    // }
+
+//4------------------------------------------------------------------------------
+    let mut how_many = String::new();
+    println!("How many random numbers you want to generate?");
+    io::stdin()
+        .read_line(&mut how_many)
+        .expect("Error reading input");
+    
+    let num_guess = how_many.trim().parse().expect("Error reading input");
+
+    let mut correct = Vec::new();
+
+    for _ in 0..num_guess {
+        correct.push(rand::thread_rng().gen_range(1..=10));
+    }
+
+    // println!("Correct: {correct:?}");
+
+    let mut guesses_made = 0;
+
     println!("Hey, guess a number 1-10:");
-    loop {
+    while guesses_made < num_guess {
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -56,14 +100,21 @@ fn main() {
             }
         };
     
-        match guess.cmp(&correct) {
+        match guess.cmp(&correct[guesses_made as usize]) {
             Ordering::Greater => println!("You guessed too high."),
             Ordering::Less => println!("You guessed too low."),
             Ordering::Equal => {
                 println!("You guessed CORRECT");
-                break;
+                guesses_made += 1;
+                if guesses_made < num_guess {
+                    println!("Let's now try the next number!");
+                }
             }  
         };
     }
+    println!("Thanks for playing! The correct answers were:");
+    for item in correct {
+        println!("{item}")
+    } 
 
 }
